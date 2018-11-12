a024d93b0ae386a3325933985571f26069a6dee0
Ruslan <denbingon@gmail.com>
2018-11-12 21:38:17 +0300

    Add auth form and select wanted tls connect

--- mail/new/main.py
+++ mail/new/main.py
@@ -3,6 +3,7 @@ import sys
 from gui import *
 from PyQt5 import *
 from datetime import datetime
+tls = 0
 class MyWin(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
@@ -10,11 +11,18 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.MailPost)
         self.ui.progressBar.setValue(0)
-        self.ui.textBrowser_2.append('Program is start ' + datetime.strftime(datetime.now(), "%H:%M:%Sn"))
+        self.ui.textBrowser_2.append('Program is start ' + datetime.strftime(datetime.now(), "%H:%M:%S"))
+        self.ui.checkBox.stateChanged.connect(self.TLS)
+    def TLS(self):
+        tls = 1
     def MailPost(self):
         self.ui.textBrowser_2.append('Search a text ' + datetime.strftime(datetime.now(), "%H:%M:%S"))
         kolvo = 0
         scro = 1
+        smtp = self.ui.lineEdit_3.text()
+        smtp_p = self.ui.lineEdit_5.text()
+        user = self.ui.lineEdit_4.text()
+        passw = self.ui.lineEdit_6.text()
         base_mail = self.ui.textEdit.toPlainText()
         mail_post = self.ui.lineEdit.text()
         title = self.ui.lineEdit_2.text()
@@ -25,9 +33,10 @@ class MyWin(QtWidgets.QMainWindow):
         for kol in kolv:
             kolvo += 1
         self.ui.textBrowser_2.append('Запускаю функцию отправки ' + datetime.strftime(datetime.now(), "%H:%M:%S"))
-        smtpObj = smtplib.SMTP("ENTER SMTP",PORT)
-        smtpObj.starttls()
-        smtpObj.login("ENTER MAIL","ENTER PASSWORD")
+        smtpObj = smtplib.SMTP(smtp, smtp_p)
+        if tls == 1:
+            smtpObj.starttls()
+        smtpObj.login(user, passw)
         for mail in kolv:
             smtpObj.sendmail(mail_post, mail, text)
             progress = int(scro/kolvo*100)
